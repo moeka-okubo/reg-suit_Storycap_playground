5c20a4af26e9dd3cfd2cb2e2180e580fa1595d8b
moeka-okubo <moeka.okubo@arsaga.jp>
2024-05-21 21:08:03 +0900

    一旦push

--- storycap.config.js
+++ storycap.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+const config = {
   storybookUrl: 'http://localhost:6006',
   outDir: '__screenshots__',
-  delay: 1000,
-  captureTimeout: 60,
+  delay: 2000,
+  captureTimeout: 100,
 };
+
+export default config;
